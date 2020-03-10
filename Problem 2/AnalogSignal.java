961e89e36e5da48edf72e960ae14daa66975d7bb
syedali02 <flawless.ali4u@gmail.com>
2020-03-10 12:24:24 -0500

    Add files via upload

---
+++ Problem 2/AnalogSignal.java
@@ -0,0 +1,8 @@
+package problem2;
+
+public interface AnalogSignal
+{
+    float[] getAnalog();
+    void setAnalog(float[] data);
+    String sendAnalog();
+}
---
+++ Problem 2/DigitalSignal.java
@@ -0,0 +1,8 @@
+package problem2;
+
+public interface DigitalSignal
+{
+    byte[] getDigital();
+    void setDigital(byte[] data);
+    String sendDigital();
+}
